0b76a57632395e0c2790a2e1f7021aa759e2735c
Diwakar Bhatt <diwakar.bhatt@cloudwords.com>
2017-04-29 10:48:36 +0530

    Initial Commit

---
+++ src/main/java/com/ttn/DemospringsecurityApplication.java
@@ -0,0 +1,12 @@
+package com.ttn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringsecurityApplication.class, args);
+	}
+}
